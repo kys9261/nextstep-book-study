4608de44cd771605048a1c96f5709243224507bc
Mike Kim <nser789@gmail.com>
2019-04-01 18:00:53 +0900

    4차 요구사항 적용 완료

--- web-application-server-study/src/main/java/webserver/RequestHandler.java
+++ web-application-server-study/src/main/java/webserver/RequestHandler.java
@@ -3,12 +3,14 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,15 +34,40 @@ public class RequestHandler extends Thread {
             }
 
             String url = HttpRequestUtils.getUrl(line);
-            if(url.startsWith("/user/create")) {
-                Map<String, String> queryStringMap = HttpRequestUtils.parseQueryString(HttpRequestUtils.getQueryString(url));
-                User user = new User(queryStringMap.get("userId"), queryStringMap.get("password"), queryStringMap.get("name"), queryStringMap.get("email"));
+
+            Map<String, String> headerMap = new HashMap<String, String>();
+            while(!"".equals(line)) {
+                line = bufferedReader.readLine();
+
+                String[] splitedHeader = line.split(": ");
+                if(splitedHeader.length == 2) {
+                    headerMap.put(splitedHeader[0], splitedHeader[1]);
+                }
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if(url.startsWith("/user/create")) {
+                String bodyData = IOUtils.readData(bufferedReader, Integer.parseInt(headerMap.get("Content-Length")));
+                Map<String, String> queryStringMap = HttpRequestUtils.parseQueryString(bodyData);
+                User user = new User(queryStringMap.get("userId"), queryStringMap.get("password"), queryStringMap.get("name"), queryStringMap.get("email"));
+
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos);
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: /index.html\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
